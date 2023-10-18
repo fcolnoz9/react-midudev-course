d644a0976d6c53d2807dc46a6e1b1faac3056603
fcolmenarez_technisys <fcolmenarez@technisys.com>
2023-10-18 20:36:00 -0300

    feat(): first commit of twitterFollowCard

---
+++ projects/00-hello-world/src/App.jsx
@@ -0,0 +1,6 @@
+import './App.css';
+import {TwitterFollowCard} from './TwitterFollowCard.jsx';
+
+export function App() {
+	return (<TwitterFollowCard userName="fernandocolnoz" name="Fernando Colmenarez"/>);
+}
