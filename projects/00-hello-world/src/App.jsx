a1b27bd435637e6e52c90adfa99e925d30f8202a
fcolmenarez_technisys <fcolmenarez@technisys.com>
2023-10-19 14:54:40 -0300

    fix(): render with a list of users

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -2,5 +2,47 @@ import './App.css';
 import {TwitterFollowCard} from './TwitterFollowCard.jsx';
 
 export function App() {
-	return (<TwitterFollowCard userName="fernandocolnoz" name="Fernando Colmenarez"/>);
+
+	const users =[
+		{
+			userName: 'fernandocolnoz',
+			name: "Fernando Colmenarez",
+			isFollowing : true
+		}
+		,
+		{
+			userName: 'fernandocolnoz',
+			name: "Fernando Oronoz",
+			isFollowing : false
+		}
+		,
+		{
+			userName: 'fernandocolnoz',
+			name: "Fernando Colnoz",
+			isFollowing : false
+		},
+		{
+			userName: 'fernandocolnoz',
+			name: "Fernando Nozcol",
+			isFollowing : true
+		},
+		{
+			userName: 'fernandocolnoz',
+			name: "Fernando Colmenarez",
+			isFollowing : false
+		}
+	]
+
+
+	return (
+		<section className="App">
+			{
+				users.map(({userName, isFollowing, name}) => (
+					<TwitterFollowCard key={userName} userName={userName} initialIsFollowing={isFollowing}>
+						{name}
+					</TwitterFollowCard>
+				))
+			}
+		</section>
+	);
 }
--- projects/00-hello-world/src/TwitterFollowCard.jsx
+++ projects/00-hello-world/src/TwitterFollowCard.jsx
@@ -1,6 +1,23 @@
+import {useState} from 'react';
 
-export function TwitterFollowCard({userName, name, isFollowing}) {
+// eslint-disable-next-line react/prop-types
+export function TwitterFollowCard({ children, userName= 'unknown', initialIsFollowing}) {
+
+	const [isFollowing, setIsFollowing ] =  useState(initialIsFollowing);
+
+	const text = isFollowing
+		? 'Siguiendo'
+		: 'Seguir';
+
+	const buttonClassName = isFollowing
+		? 'tw-followCard-button is-following'
+		: 'tw-followCard-button';
+
 	const imageSrc = `https://unavatar.io/${userName}`;
+
+	const handleClick = () => {
+		setIsFollowing(!isFollowing);
+	};
 	return (
 		<article className="tw-followCard">
 			<header className="tw-followCard-header">
@@ -9,14 +26,14 @@ export function TwitterFollowCard({userName, name, isFollowing}) {
 					alt={`El avatar de ${userName}`}
 					src={imageSrc}/>
 				<div className="tw-followCard-info">
-					<strong>{name}</strong>
+					<strong>{children}</strong>
 					<span className="tw-followCard-infoUserName">@{userName}</span>
 				</div>
 			</header>
 
 			<aside>
-				<button className="tw-followCard-button">
-					Seguir
+				<button className={buttonClassName} onClick={handleClick}>
+					{text}
 				</button>
 			</aside>
 		</article>);
